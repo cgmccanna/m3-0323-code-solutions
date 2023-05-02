84a229427c391cddc6704bf07eb7f9536db2ab00
Charlotte McCanna <charliegmccanna@gmail.com>
2023-05-02 02:07:24 +0000

    Completed fetch exercise.

--- fetch/main.js
+++ fetch/main.js
@@ -1,5 +1,5 @@
 async function logData() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/usrs', {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users', {
     method: 'GET'
   });
   if (!response.ok) {
